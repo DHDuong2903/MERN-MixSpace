313be060fca4a1e710da9c7ffce67e06f0521d1d
DHDuong2903 <dohuyduong2903@gmail.com>
2025-04-14 22:18:35 +0700

    config_cloudinary_and_handle_create_song

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { clerkMiddleware } from "@clerk/express";
+import { fileUpload } from "express-fileupload";
+import path from "path";
 
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -12,12 +14,22 @@ import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json()); // To parse req.body
-
 app.use(clerkMiddleware()); // This will add auth to req obj => req.auth.userId
+app.use(
+  fileUpload({
+    useTemFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024, // 10MB max file size
+    },
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
@@ -25,6 +37,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
