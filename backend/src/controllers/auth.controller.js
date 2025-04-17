e615381a8d1c37c61bc8104f3938a9b492b4e238
DHDuong2903 <dohuyduong2903@gmail.com>
2025-04-17 20:49:57 +0700

    handle_login_and_register_by_clerk

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import { User } from "../models/user.model.js";
 export const authCallback = async (req, res, next) => {
   try {
     const { id, firstName, lastName, imageUrl } = req.body;
+    console.log(">>> req.body", req.body);
 
     // Check if user already exists
     const user = await User.findOne({ clerkId: id });
@@ -11,7 +12,7 @@ export const authCallback = async (req, res, next) => {
       // register
       await User.create({
         clerkId: id,
-        fullName: `${firstName} ${lastName}`,
+        fullName: `${firstName || ""} ${lastName || ""}`,
         imageUrl,
       });
     }
@@ -19,6 +20,6 @@ export const authCallback = async (req, res, next) => {
     res.status(200).json({ success: true });
   } catch (error) {
     console.log("Error in auth callback", error);
-    next(err);
+    next(error);
   }
 };
---
+++ frontend/src/providers/AuthProvider.tsx
@@ -0,0 +1,48 @@
+import { axiosInstance } from "@/lib/axios";
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+import { Loader } from "lucide-react";
+
+// Cập nhật Authorization header trong axiosInstance.
+const updateApiToken = (token: string | null) => {
+  if (token) {
+    axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`; // Nếu có token thì thêm vào header dưới dạng Bearer token.
+  } else {
+    delete axiosInstance.defaults.headers.common["Authorization"]; // Nếu không có thì xóa header đi.
+  }
+};
+
+// Đây là một wrapper component, nhận children (các thành phần con) và bao bọc bên ngoài để đảm bảo rằng xác thực được xử lý trước khi render các thành phần con.
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const { getToken, userId } = useAuth();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const initAuth = async () => {
+      try {
+        const token = await getToken(); // Lấy token từ Clerk
+        updateApiToken(token); // Gắn token vào axios header
+      } catch (error: any) {
+        updateApiToken(null); // Nếu lỗi thì xóa token
+        console.log("Error in auth provider", error);
+      } finally {
+        setLoading(false); // Kết thúc loading
+      }
+    };
+
+    initAuth();
+  }, [getToken, userId]);
+
+  if (loading) {
+    return (
+      <div className="h-screen w-full flex items-center justify-center">
+        <Loader className="size-8 text-emerald-500 animate-spin" />{" "}
+        {/* Icon Loader dùng để hiển thị trạng thái loading với hiệu ứng quay (animate-spin). */}
+      </div>
+    );
+  }
+
+  return <>{children}</>;
+};
+
+export default AuthProvider;
